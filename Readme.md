019e065e1579c6d6d2466afb61d65a58df7dea81
Lorenzo Stanco <lorenzo.stanco@gmail.com>
2012-04-15 19:51:42 +0200

    First commit. Fully functional already.

---
+++ Readme.md
@@ -0,0 +1,63 @@
+
+**Minixed** is a minimal but nice-looking PHP directory indexer.  
+You can see it as a replacement for the Apache `mod_autoindex`.
+
+> *«How does it look?»* Like [**this**](http://www.lorenzostanco.com/minixed_demo/).  
+> *«I don't like it.»* You are a CSS master, right? Good, you know what to do.
+
+How to use
+----------
+
+Just drop the [**`index.php`**](https://github.com/lorenzos/Minixed/blob/master/index.php) 
+script in the same directory that contains files you want to index.
+
+> *«Really? One single file?»* Yes, really. Glad you asked.  
+> *«If so, where are icon files?»* They are hard coded into the source, thanks Base-64.  
+> *«Was it really necessary?»* I don't know, I just loved the idea you need only one file.
+
+If you want Minixed to work also in subfolders, you need to place a copy of `index.php`
+in every directory you want to index.
+
+Configuration
+-------------
+
+The script works well out-of-the-box, and generally you want to leave it as it is.
+
+However if you have some particular needs, there are some PHP variables placed 
+in the first lines of `index.php` you can edit.
+
+You can change the page title (and subtitle) providing strings that can contains
+some placeholders that will be *parsed* at runtime:
+
+	$title = 'Index of {{path}}';
+	$subtitle = '{{files}} objects in this folder, {{size}} total'; // Empty to disable
+	
+You can tell the script how to build the files list using:
+	
+	$showParent = false; // Display a (parent directory) link
+	$showDirectories = true;
+	$showHiddenFiles = false; // Display files starting with "." too
+	
+And how that list should look:
+
+	$alignment = 'left'; // You can use 'left' or 'center'
+	$showIcons = true;
+	$dateFormat = 'dd/mm/yyyy HH:ii'; // Used in date() function
+	$sizeDecimals = 1;
+	
+Finally, you can customize the content of the meta-tag "robots" 
+if you want to give some search engine hints:
+
+	$robots = 'noindex, nofollow'; // Avoid robots by default
+	
+Of course, if PHP is a friend of yours you can easily
+understand the whole script source code, so the only limit to
+customization is your imagination.
+
+Bug tracking and developing
+---------------------------
+
+If you find bugs, if you have suggestions, if you modified the script adding 
+features or improvements, feel free to contribute by opening 
+[**Issues**](https://github.com/lorenzos/Minixed/issues) or 
+[**Pull Requests**](https://github.com/lorenzos/Minixed/fork).
---
+++ index.php
@@ -0,0 +1,335 @@
+<?php
+
+	// MINIXED is a minimal but nice-looking PHP directory indexer.
+	// More at https://github.com/lorenzos/Minixed
+
+	// =============================
+	// Configuration                
+	// =============================
+	
+	$title = 'Index of {{path}}';
+	$subtitle = '{{files}} objects in this folder, {{size}} total'; // Empty to disable
+	$showParent = false; // Display a (parent directory) link
+	$showDirectories = true;
+	$showHiddenFiles = false; // Display files starting with "." too
+	$alignment = 'left'; // You can use 'left' or 'center'
+	$showIcons = true;
+	$dateFormat = 'd/m/y H:i'; // Used in date() function
+	$sizeDecimals = 1;
+	$robots = 'noindex, nofollow'; // Avoid robots by default
+	$showFooter = true; // Display the "Powered by" footer
+	
+	// =============================
+	// =============================
+	
+	// Who am I?
+	$_self = basename($_SERVER['PHP_SELF']);
+	$_path = str_replace('\\', '/', dirname($_SERVER['PHP_SELF']));
+	$_total = 0;
+	$_total_size = 0;
+	
+	// Encoded images generator
+	if (!empty($_GET['i'])) {
+		header('Content-type: image/png');
+		switch ($_GET['i']) {
+			case       'asc': exit(base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAFUlEQVQImWNgoBT8x4JxKsBpAhUAAPUACPhuMItPAAAAAElFTkSuQmCC'));
+			case      'desc': exit(base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAF0lEQVQImWNgoBb4j0/iPzYF/7FgCgAADegI+OMeBfsAAAAASUVORK5CYII='));
+			case 'directory': exit(base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAASklEQVQYlYWPwQ3AMAgDb3Tv5AHdR5OqTaBB8gM4bAGApACPRr/XuujA+vqVcAI3swDYjqRSH7B9oHI8grbTgWN+g3+xq0k6TegCNtdPnJDsj8sAAAAASUVORK5CYII='));
+			case      'file': exit(base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAPklEQVQYlcXQsQ0AIAhE0b//GgzDWGdjDCJoKck13CsIALi7gJxyVmFmyrsXLHEHD7zBmBbezvoJm4cL0OwYouM4O3J+UDYAAAAASUVORK5CYII='));
+		}
+	}
+	
+	// I'm not sure this function is really needed...
+	function ls($path, $show_folders = false, $show_hidden = false) {
+		global $_self, $_total, $_total_size;
+		$ls = array();
+		$ls_d = array();
+		if (($dh = @opendir($path)) === false) return $ls;
+		if (substr($path, -1) != '/') $path .= '/';
+		while (($file = readdir($dh)) !== false) {
+			if ($file == $_self) continue;
+			if ($file == '.' || $file == '..') continue;
+			if (!$show_hidden) if (substr($file, 0, 1) == '.') continue;
+			$isdir = is_dir($path . $file);
+			if (!$show_folders && $isdir) continue;
+			$item = array('name' => $file, 'isdir' => $isdir, 'size' => $isdir ? 0 : filesize($path . $file), 'time' => filemtime($path . $file));
+			if ($isdir) $ls_d[] = $item; else $ls[] = $item;
+			$_total++;
+			$_total_size += $item['size'];
+		}
+		return array_merge($ls_d, $ls);
+	}
+	
+	// Get the list of files
+	$items = ls('.', $showDirectories, $showHiddenFiles);
+	
+	// Sort it
+	function sortByName($a, $b) { return ($a['isdir'] == $b['isdir'] ? strtolower($a['name']) > strtolower($b['name']) : $a['isdir'] < $b['isdir']); }
+	function sortBySize($a, $b) { return ($a['size'] > $b['size']); }
+	function sortByTime($a, $b) { return ($a['time'] > $b['time']); }
+	switch ($_GET['s']) {
+		case 'size': $_sort = 'size'; usort($items, 'sortBySize'); break;
+		case 'time': $_sort = 'time'; usort($items, 'sortByTime'); break;
+		default    : $_sort = 'name'; usort($items, 'sortByName'); break;
+	}
+	
+	// Reverse?
+	$_sort_reverse = ($_GET['r'] == '1');
+	if ($_sort_reverse) $items = array_reverse($items);
+	
+	// Add parent
+	if ($showParent && $_path != '/') array_unshift($items, array(
+		'name' => '..',
+		'isparent' => true,
+		'isdir' => true,
+		'size' => 0,
+		'time' => 0
+	));
+	
+	// 37.6 MB is better than 39487001
+	function humanizeFilesize($val, $round = 0) {
+		$unit = array('','K','M','G','T','P','E','Z','Y');
+		do { $val /= 1024; array_shift($unit); } while ($val >= 1000);
+		return sprintf('%.'.intval($round).'f', $val) . ' ' . array_shift($unit) . 'B';
+	}
+	
+	// Titles parser
+	function getTitle($title) {
+		global $_path, $_total, $_total_size, $sizeDecimals;
+		return str_replace(array('{{path}}', '{{files}}', '{{size}}'), array($_path, $_total, humanizeFilesize($_total_size, $sizeDecimals)), $title);
+	}
+	
+	// Link builder
+	function buildLink($changes) {
+		global $_self;
+		$params = $_GET;
+		foreach ($changes as $k => $v) if (is_null($v)) unset($params[$k]); else $params[$k] = $v;
+		foreach ($params as $k => $v) $params[$k] = urlencode($k) . '=' . urlencode($v);
+		return empty($params) ? $_self : $_self . '?' . implode($params, '&');
+	}
+
+?>
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	
+	<meta charset="UTF-8">
+	<meta name="robots" content="<?php echo htmlentities($robots) ?>">
+	
+	<title><?php echo htmlentities(getTitle($title)) ?></title>
+	
+	<style type="text/css">
+		
+		* {
+			margin: 0;
+			padding: 0;
+			border: none;
+		}
+		
+		body {
+			text-align: center;
+			font-family: sans-serif;
+			font-size: 13px;
+			color: #000000;
+		}
+		
+		#wrapper {
+			width: 600px;
+			margin: 0 auto;
+			text-align: left;
+		}
+		
+		body#left {
+			text-align: left;
+		}
+		
+		body#left #wrapper {
+			margin: 0 20px;
+		}
+		
+		h1 {
+			font-size: 21px;
+			padding: 0 10px;
+			margin: 20px 0 0;
+			font-weight: bold;
+		}
+		
+		h2 {
+			font-size: 14px;
+			padding: 0 10px;
+			margin: 10px 0 0;
+			color: #999999;
+			font-weight: normal;
+		}
+		
+		a {
+			text-decoration: none;
+		}
+		
+		ul#header {	
+			margin-top: 20px;
+		}
+		
+		ul li {
+			display: block;
+			list-style-type: none;
+			overflow: hidden;
+			padding: 10px;
+		}
+		
+		ul li:hover {
+			background-color: #f3f3f3;
+		}
+		
+		ul li .date {
+			text-align: center;
+			width: 120px;
+		}
+		
+		ul li .size {
+			text-align: right;
+			width: 90px;
+		}
+		
+		ul li .date, ul li .size {
+			float: right;
+			font-size: 12px;
+			display: block;
+			color: #666666;
+		}
+		
+		ul#header li {
+			font-size: 11px;
+			font-weight: bold;
+			border-bottom: 1px solid #cccccc;
+		}
+		
+		ul#header li:hover {
+			background-color: transparent;
+		}
+		
+		ul#header li * {
+			color: #000000;
+			font-size: 11px;
+		}
+		
+		ul#header li a:hover {
+			color: #666666;
+		}
+		
+		ul#header li .asc span, ul#header li .desc span {
+			padding-right: 15px;
+			background-position: right center;
+			background-repeat: no-repeat;
+		}
+		
+		ul#header li .asc span {
+			background-image: url('<?php echo $_self ?>?i=asc');
+		}
+		
+		ul#header li .desc span {
+			background-image: url('<?php echo $_self ?>?i=desc');
+		}
+		
+		ul li.item {
+			border-top: 1px solid #f3f3f3;
+		}
+		
+		ul li.item:first-child {
+			border-top: none;
+		}
+		
+		ul li.item .name {
+			color: #003399;
+			font-weight: bold;
+		}
+		
+		ul li.item .name:hover {
+			color: #0066cc;
+		}
+		
+		ul li.item a:hover {
+			text-decoration: underline;
+		}
+		
+		ul li.item .directory, ul li.item .file {
+			padding-left: 20px;
+			background-position: left center;
+			background-repeat: no-repeat;
+		}
+		
+		ul li.item .directory {
+			background-image: url('<?php echo $_self ?>?i=directory');
+		}
+		
+		ul li.item .file {
+			background-image: url('<?php echo $_self ?>?i=file');
+		}
+		
+		#footer {
+			color: #cccccc;
+			font-size: 11px;
+			margin-top: 40px;
+			padding: 0 10px;
+			text-align: left;
+		}
+		
+		#footer a {
+			color: #cccccc;
+			font-weight: bold;
+		}
+		
+		#footer a:hover {
+			color: #999999;
+		}
+		
+	</style>
+	
+</head>
+<body <?php if ($alignment == 'left') echo 'id="left"' ?>>
+
+	<div id="wrapper">
+		
+		<h1><?php echo htmlentities(getTitle($title)) ?></h1>
+		<h2><?php echo htmlentities(getTitle($subtitle)) ?></h2>
+		
+		<ul id="header">
+			
+			<li>
+				<a href="<?php echo buildLink(array('s' => 'size', 'r' => (!$_sort_reverse && $_sort == 'size') ? '1' : null)) ?>" class="size <?php if ($_sort == 'size') echo $_sort_reverse ? 'desc' : 'asc' ?>"><span>Size</span></a>
+				<a href="<?php echo buildLink(array('s' => 'time', 'r' => (!$_sort_reverse && $_sort == 'time') ? '1' : null)) ?>" class="date <?php if ($_sort == 'time') echo $_sort_reverse ? 'desc' : 'asc' ?>"><span>Last modified</span></a>
+				<a href="<?php echo buildLink(array('s' =>  null , 'r' => (!$_sort_reverse && $_sort == 'name') ? '1' : null)) ?>" class="name <?php if ($_sort == 'name') echo $_sort_reverse ? 'desc' : 'asc' ?>"><span>Name</span></a>
+			</li>
+			
+		</ul>
+		
+		<ul>
+			
+			<?php foreach ($items as $item): ?>
+				
+				<li class="item">
+				
+					<span class="size"><?php echo $item['isdir'] ? '-' : humanizeFilesize($item['size'], $sizeDecimals) ?></span>
+					
+					<span class="date"><?php echo ($item['isparent']) ? '-' : date($dateFormat, $item['time']) ?></span>
+					
+					<a href="<?php echo htmlentities($item['name']) ?>" class="name <?php if ($showIcons) echo $item['isdir'] ? 'directory' : 'file' ?>"><?php echo htmlentities($item['name']) . ($item['isdir'] ? ' /' : '') ?></a>
+					
+				</li>
+				
+			<?php endforeach; ?>
+			
+		</ul>
+		
+		<?php if ($showFooter): ?>
+			
+			<p id="footer">
+				Powered by <a href="https://github.com/lorenzos/Minixed" target="_blank">Minixed</a>, a PHP directory indexer
+			</p>
+			
+		<?php endif; ?>
+		
+	</div>
+	
+</body>
+</html>
